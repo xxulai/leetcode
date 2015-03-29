25d556413681266aed456328af515b934d36cec1
xxulai <xulai_taobao@hotmail.com>
2015-03-29 22:13:01 +0800

    Work weekend

--- leetcode/leetcode/MediumCase.h
+++ leetcode/leetcode/MediumCase.h
@@ -3,7 +3,7 @@
 class MediumCase{
 public:
       vector<vector<int>> pathSumII(TreeNode *root, int sum); //Path Sum II
-
+	  int minimumTotal(vector<vector<int> > &triangle); //Triangle 
 private: 
 	//static void getPath(TreeNode *leaf, vector<int> &list, vector<vector<int>> &retlist);
 };
